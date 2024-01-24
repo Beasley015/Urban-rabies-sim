391a81f8df09412c4673fd02ab7ae7af552e9db5
Beasley015 <beasley.em@gmail.com>
2024-01-24 11:34:26 -0500

    add functions script

---
+++ Functions.jl
@@ -0,0 +1,518 @@
+# Create simulated landscapes
+function initialize_land(;land_size = 60, barrier_strength=0, habitats)
+    # 1 cell = 0.5 km by 0.5 km
+    # 5-cell buffer in outputs
+
+    # Create neutral landscape based on real habitat proportions
+    clustered_land = rand(NearestNeighborCluster(0.2), (land_size, land_size))
+    landscape = NeutralLandscapes.classify(clustered_land, land_proportions[2:9])
+
+    if barrier_strength != 0
+        # Pick starting and ending edges for barrier
+        edges = ["north", "south", "east", "west"]
+        barrier1edge = sample(edges, 2, replace=false)
+        barrier2edge = sample(edges, 2, replace=false)
+
+        function create_coord(x)
+            y=Vector(undef, 2)
+
+            if x == "north"
+                y[1] = rand(1:land_size)
+                y[2] = land_size
+            elseif x == "east"
+                y[1] = land_size
+                y[2] = rand(1:land_size)
+            elseif x == "south"
+                y[1] = rand(1:land_size)
+                y[2] = 1
+            else 
+                y[1] = 1
+                y[2] = rand(1:land_size)
+            end
+
+            return y
+        end
+        
+        # Create start and end points of landscape barriers
+        barrier1start = create_coord(barrier1edge[1])
+        barrier1end = create_coord(barrier1edge[2])
+
+        barrier2start = create_coord(barrier2edge[1])
+        barrier2end = create_coord(barrier2edge[2])
+
+        function add_barriers(startcoords, endcoords)
+            # Calculate slope of barrier line
+            slope = (startcoords[2]-endcoords[2])/(startcoords[1]-endcoords[1])
+
+            # Intercept of barrier
+            intercept = startcoords[2] - slope*startcoords[1]
+
+            # Get the cells the barrier falls un
+            x = collect(range(start=minimum([startcoords[1], endcoords[1]]), stop=maximum([startcoords[1],endcoords[1]]), step=0.1))
+            y = (slope .* x) .+ intercept
+
+            barrier = DataFrame(x = trunc.(Int,round.(x)), y = trunc.(Int, round.(y)))
+            barrier = unique(barrier)
+
+            return barrier
+        end
+        
+        # Create barriers
+        barrier1 = add_barriers(barrier1start, barrier1end)
+        barrier2 = add_barriers(barrier2start, barrier2end)
+        
+        # Update habitat type at barrier location
+        landscape[CartesianIndex.(barrier1.x, barrier1.y)] .= 9.0
+        landscape[CartesianIndex.(barrier2.x, barrier2.y)] .= 9.0
+
+        # Update movement coefficients for barrier
+        barrier_stren = [1,2,3,4]
+        barrier_coefs = [-0.25, -0.5, -1, -10]
+
+        barrier_frame = DataFrame(strength=barrier_stren, coef = barrier_coefs)
+        barrier_index = findall(x-> x == barrier_strength, barrier_frame.strength)
+        coef = barrier_frame.coef[barrier_index]
+
+        habitats[9,:] = ["barrier", 0.0, coef[1]]
+    end
+
+    return landscape
+end
+
+# Initialize raccoon populations
+function populate_landscape(;land_size = 60, guy_density = 2.75, seros)
+    # Define corners of landscape
+    xmin = 1; xmax = land_size
+    ymin = 1; ymax = land_size
+    land_area = xmax*ymax
+
+    # get number of guys based on landscape size & density
+    nguys = rand(Poisson(land_area*guy_density))
+
+    # Create data frame of guys
+    lil_guys = DataFrame(id = string.(collect(1:nguys)), x = convert.(Int64,trunc.(xmax*rand(nguys,1)))[:,1],
+        y = convert.(Int64,trunc.(ymax*rand(nguys,1)))[:,1], incubation = 0, time_since_inf = 0, infectious = 0, time_since_disease = 0,
+        sex = Int.(rand(Bernoulli(0.5), nguys)), mom = NaN, vaccinated = 0, age = rand(52:(52*8), nguys))
+
+    # Remove 0's 
+    lil_guys.x[lil_guys.x .== 0] .= 1
+    lil_guys.y[lil_guys.y .== 0] .= 1
+
+    # Initialize disease
+    lil_guys.incubation[rand(1:nguys,convert(Int64,round(nguys * 0.01)))] .= 1
+    lil_guys.time_since_inf = ifelse.(lil_guys.incubation .== 1, 1, lil_guys.time_since_inf)
+
+    # Initialize immunity
+    lil_guys.vaccinated[lil_guys.incubation .!= 1] = rand(Bernoulli(seros), length(lil_guys.vaccinated[lil_guys.incubation .!= 1]))
+
+    return lil_guys
+end
+
+# Create function for the guys to look at their surroundings
+function look_around(x,y,land_size)
+    # Get vector of all moves
+    all_moves = [(x-1, y+1), (x, y+1), (x+1, y+1),
+                (x-1, y), (x,y), (x+1, y),
+                (x-1, y-1), (x, y-1), (x+1, y-1)]
+
+    # Get indices of impossible moves
+    good_moves = findall([(0 .< x[1] .<= land_size) .&& (0 .< x[2] .<= land_size) for x in all_moves])
+
+    # Remove impossible moves
+    poss_moves = deepcopy(all_moves[good_moves])
+
+    return poss_moves
+end
+
+# Movement and disease function
+function move(coords, dat, home, landscape, reso=500, rate=-0.5)
+    # Where coords = list of tuples representing possible moves,
+    # dat = data frame of agents,
+    # reso = width/height of grid cell in meters
+    # rate = rate of distance-decay. Based on trial and error so raccoons typically stay ~1km from home
+
+    # Create blank array
+    habs = Vector(undef, length(coords))
+
+    # Get habitat type to create weights
+    for i in 1:length(coords)
+        habs[i] = landscape[CartesianIndex.([x[1] for x in coords[i]], [x[2] for x in coords[i]])]
+    end
+
+    # Match habitats to McClure coefficients
+    hab_prefs = Vector(undef, length(coords))
+    for i in 1:length(coords)
+        hab_prefs[i] = hab_frame.coef[convert.(Int64, habs[i])]
+    end
+   
+    # Movement weights as a function of distance from initial coords
+    distances = Vector(undef, length(coords))
+    dist_weights = Vector(undef, length(coords))
+    for i in 1:length(coords)
+        home_loc = (home.x[i], home.y[i]) 
+        distances[i] = ((([x[1] for x in coords[i]].-home_loc[1]).^2 .+ ([x[2] for x in coords[i]].-home_loc[2]).^2)*reso)/100
+        dist_weights[i] = exp.(rate .* distances[i])
+    end
+
+    # Combine habitat and distance-decay weights
+    weights = Vector(undef, length(dist_weights))
+    for i in 1:length(dist_weights)
+        weights[i] = dist_weights[i] .* hab_prefs[i]
+    end
+
+    # Choose new location
+    new_location = sample.(coords, Weights.(weights))
+
+    # Make it a df
+    new_spots = DataFrame(x = [x[1] for x in new_location], y = [x[2] for x in new_location])
+
+    # Update data frame
+    dat.x = deepcopy(new_spots.x)
+    dat.y = deepcopy(new_spots.y)
+
+    # kids follow mom
+    kids = findall(dat.age .< 20)
+
+    # Sometimes mom died, so need to filter orphans out to murder later
+    poss_indices = [findall(dat.id .== dat.mom[kids][i]) for i in 1:length(kids)]
+    blanks = findall(isempty.(poss_indices) .== 1)
+    deleteat!(kids, blanks)
+    
+    indices = vcat(poss_indices...)
+
+    # New kid coords
+    x_coords = deepcopy(dat.x[indices])
+    y_coords = deepcopy(dat.y[indices])
+
+    dat.x[kids] = deepcopy(x_coords)
+    dat.y[kids] = deepcopy(y_coords)
+  
+    # Disease transmission
+    if length(filter(kv -> kv.second > 1, countmap(new_location))) > 0
+        # get coordinates where there are multiple guys
+        many_guys = collect(keys(filter(kv -> kv.second > 1, countmap(new_location))))
+        indices = [findall(==(x), new_location) for x in many_guys]
+
+        # spread disease
+        diseases = deepcopy(dat.infectious)
+        vax_rows = findall(x->x==1, dat.vaccinated)
+
+        for i in 1:length(indices)
+            # Remove vaccinated guys
+            to_remove = findall(in(intersect(vax_rows, indices[i])), indices[i])
+            indices[i] = ifelse(length(to_remove) > 0, deleteat!(indices[i], to_remove), indices[i])
+
+            # spread disease to unvaccinated guys
+            if any(diseases[indices[i]] .== 1)
+                test = diseases[indices[i]]
+                test[test .!= 1].= rand(Bernoulli(0.3), length(test[test .!= 1]))
+
+                diseases[indices[i]] = test
+            end  
+        end
+
+        # Keep existing infections
+        diseases[findall(dat.incubation .== 1)] .= 1
+        
+        dat.incubation = deepcopy(diseases) 
+
+    end 
+end
+
+# Transition from incubation period to infectious period
+function begin_symptoms(dat)
+    # Probability of transition
+    prob = rand.(Beta.(dat.time_since_inf[(dat.incubation .== 1) .& (dat.infectious .== 0)] .+ 0.000000000001,5))
+    # If the probability is 0 it does not work; so add a miniscule number
+
+    if length(prob) > 0
+        dat.infectious[(dat.incubation .== 1) .& (dat.infectious .== 0)] = reduce(vcat,rand.(Bernoulli.(prob), 1))
+    end
+end
+
+# Reproduction function
+function reproduce(dat, home)
+    # Get females only
+    females = filter(:sex => ==(1), dat)
+    
+    # Choose females that actually reproduce
+    reproducing = females[randsubseq(1:size(females,1), 0.5),:]
+
+    # Assign number of offspring to each reproducing female
+    noffspring = rand(Poisson(2), size(reproducing,1))
+
+    # Create offspring at location of mother
+    devil_spawn = DataFrame(x = Int[], y = Int[], mom = String[])
+    for i in 1:length(noffspring)
+        for j in 1:noffspring[i]
+            push!(devil_spawn, (reproducing.x[i], reproducing.y[i], reproducing.id[i]))
+        end
+    end
+
+    # Fill in missing cols
+    devil_spawn.incubation .= 0
+    devil_spawn.time_since_inf .= 0
+    devil_spawn.infectious .= 0
+    devil_spawn.time_since_disease .= 0
+    devil_spawn.sex .= Int.(rand(Bernoulli(0.5), size(devil_spawn,1)))
+    devil_spawn.vaccinated .= 0
+    devil_spawn.age .= 0
+
+    id_vector = collect((maximum(parse.(Int64,dat.id))+1):(maximum(parse.(Int64,dat.id))+size(devil_spawn,1)))
+    devil_spawn.id = string.(id_vector)
+
+    # Append to main dataset
+    append!(dat, devil_spawn, promote = true)
+    append!(home, DataFrame(id = devil_spawn.id, x=devil_spawn.x, y=devil_spawn.y), promote = true)
+    
+end
+
+# Mortality function
+function dont_fear_the_reaper(dat, home, time=2)
+    # disease mortality set at 2 weeks, otherwise guys die before they can transmit
+    
+    # random mortality
+    rand_deaths = rand(Bernoulli(0.005),size(dat,1))
+    deleteat!(dat, findall(rand_deaths==1))
+    deleteat!(home, findall(rand_deaths==1))
+
+    # disease mortality
+    deleteat!(home, findall(x -> x > time, dat.time_since_disease))
+    filter!(:time_since_disease => <=(time), dat)
+
+    # old age mortality
+    deleteat!(home, findall(x -> x >= (52*8), dat.age))
+    if length(findall(x-> x>=(52*8), dat.age)) > 0
+        filter!(:age => <(52*8), dat)
+    end
+    
+
+    # orphan mortality
+    kids = findall(dat.age .< 20)
+
+    no_mom = findall(x -> !(x in dat.id), dat.mom[kids])
+
+    deleteat!(home, no_mom)
+    filter!(:mom => !in(dat.mom[kids][no_mom]), dat)
+
+    # Density-related mortality
+    # get coordinates where there are multiple guys
+    new_location = Vector(undef, size(dat,1))
+    for i in 1:size(dat,1)
+        new_location[i] = (dat.x[i], dat.y[i]) 
+    end
+
+    many_guys = collect(keys(filter(kv -> kv.second > 1, countmap(new_location))))
+    indices = [findall(==(x), new_location) for x in many_guys]
+
+    # Find cells with max number of guys or greater
+    too_many_guys = findall(length.(indices) .> 10) #can adjust this number
+
+    crowded_spots = many_guys[too_many_guys]
+
+    # Apply increased mortality
+    crowded_indices = Vector(undef, length(crowded_spots))
+    for i in 1:length(crowded_spots)
+        crowded_indices[i] = intersect(findall(x -> x == crowded_spots[i][1], dat.x), findall(x -> x == crowded_spots[i][2], dat.y))
+    end
+
+    crowding_deaths = sort(unique(vcat(crowded_indices[findall(rand(Bernoulli(0.1), length(crowded_indices)) .== 1)]...)))
+    
+    if length(crowding_deaths) > 0
+        deleteat!(dat, crowding_deaths)
+        deleteat!(home, crowding_deaths)
+    end
+    
+end
+
+# Vaccination function
+function ORV(;dat, land_size, land=nothing, sero_prob)
+    if land==nothing 
+        # Create array of vaccination probabilities
+        land = fill(sero_prob, (land_size,land_size))
+    end
+
+    # Get probs of vaccination at guys' locations
+    vaxprob = land[CartesianIndex.(dat.x, dat.y)]
+
+    # Juveniles have lower probs
+    kids = findall(dat.age .< 52)
+    vaxprob[kids] = vaxprob[kids]./2
+
+    # Remove raccoons that have already been vaccinated
+    novax = findall(dat.vaccinated .== 0)
+    newvax = vaxprob[novax]
+
+    # Generate vaccine outcomes
+    vax = Int.(rand.(Bernoulli.(newvax)))
+
+    # Update data
+    dat.vaccinated[novax] = vax
+
+    return dat
+end
+
+# Juvenile distribution
+function juvies_leave(dat, home, land_size, dispersal_age=30)
+    # Get Juveniles
+    juvies = dat[findall(x -> x==30, dat.age),:]
+
+    # Create break point so it doesn't get stuckin an endless loop
+    niter = 0
+
+    while size(juvies,1) > 0
+        niter = niter + 1
+
+        # Pick a direction from list of inline functions
+        upleft(x,y)=[x-1, y+1]; up(x,y)=[x, y+1]; upright(x,y)=[x+1, y+1]
+        left(x,y)=[x-1, y]; right(x,y)=[x+1, y]
+        downleft(x,y)=[x-1, y-1]; down(x,y)=[x, y-1]; downright(x,y)=[x+1, y-1]
+        directions = rand([upleft, up, upright, left, right, 
+                        downleft, down, downright], size(juvies,1))
+
+        # Get dispersal distance
+        distances = rand(Poisson(10), size(juvies,1))
+
+        # RUN!
+        coords = Vector(undef, size(juvies,1))
+
+        for i in 1:length(distances)
+            coords[i] = [juvies.x[i], juvies.y[i]]
+                for j in 1:distances[i]
+                coords[i] = directions[i](coords[i][1], coords[i][2])
+            end
+        end
+
+        # Remove juveniles that left the landscape
+        new_juvies = juvies[all.(x-> 0<x<land_size, coords),:]
+        gone_juvies = juvies[all.(x-> x<0 || x > land_size, coords),:]
+
+        # Update full data frame
+        indices=findall(x-> x in(new_juvies.id), dat.id)
+        gone_indices = findall(x-> x in(gone_juvies.id), dat.id)
+        dat[indices,:]=new_juvies
+        deleteat!(dat, gone_indices)
+
+        # Update home coords data frame
+        home.x[indices] = new_juvies.x
+        home.y[indices] = new_juvies.y
+        home.id[indices] = new_juvies.id
+        deleteat!(home, gone_indices)
+    
+        # Find coordinates with multiple guys
+        new_location = Vector(undef, size(dat,1))
+        for i in 1:size(dat,1)
+            new_location[i] = (dat.x[i], dat.y[i]) 
+        end
+    
+        many_guys = collect(keys(filter(kv -> kv.second > 1, countmap(new_location))))
+        indices = [findall(==(x), new_location) for x in many_guys]
+    
+        # Find cells with less than max number of guys
+        enough_guys = findall(length.(indices) .<= 10) #can adjust this number
+    
+        good_spots = many_guys[enough_guys]
+    
+        good_indices = Vector(undef, length(good_spots))
+        for i in 1:length(good_spots)
+            good_indices[i] = intersect(findall(x -> x == good_spots[i][1], new_juvies.x), findall(x -> x == good_spots[i][2], new_juvies.y))
+        end    
+
+        deleteat!(new_juvies,sort(unique(vcat(good_indices...))))
+
+        # Start the cycle again
+        juvies = deepcopy(new_juvies)
+
+        if niter > 5
+            break
+        end
+    end
+end
+
+# Immigration function
+function immigration(;dat, home, land_size, immigration_rate=1, sero_rate=0, disease_rate=0.05, type="propagule")
+    # get number of immigrants
+    n_new = rand(Poisson(immigration_rate))
+
+    # Data frame of immigrants
+    immigrants = DataFrame(id = string.(collect(range(start=maximum(parse.(Int, dat.id)),stop=maximum(parse.(Int, dat.id))+(n_new-1),step=1))), 
+                        x = 0, y = 0, incubation = 0, time_since_inf = 0, infectious = 0, time_since_disease = 0, 
+                        sex = Int.(rand(Bernoulli(0.5), n_new)), mom = NaN, vaccinated = 0, age = rand(52:(52*8), n_new))
+
+    # Initialize disease
+    immigrants.incubation = rand(Bernoulli(disease_rate), n_new)
+    immigrants.time_since_inf = ifelse.(immigrants.incubation .== 1, 1, immigrants.time_since_inf)
+
+    # Initialize immunity
+    immigrants.vaccinated[immigrants.incubation .!= 1] = rand(Bernoulli(sero_rate), length(immigrants.vaccinated[immigrants.incubation .!= 1]))
+
+    # Get starting edges
+    edges = ["north", "east", "south", "west"]
+
+    if type == "propagule"
+        immigrant_edges = sample(edges, n_new)
+
+        for i in 1:length(immigrant_edges)
+            if immigrant_edges[i] == "north"
+                immigrants.x[i] = rand(1:land_size)
+                immigrants.y[i] = land_size
+            elseif immigrant_edges[i] == "east"
+                immigrants.x[i] = land_size
+                immigrants.y[i] = rand(1:land_size)
+            elseif immigrant_edges[i] == "south"
+                immigrants.x[i] = rand(1:land_size)
+                immigrants.y[i] = 1
+            else 
+                immigrants.x[i] = 1
+                immigrants.y[i] = rand(1:land_size)
+            end
+        end
+    end
+
+    if type == "wave"
+        immigrant_edges = sample(edges, 1)
+
+        if immigrant_edges == "north"
+            immigrants.x = rand(1:land_size, n_new)
+            immigrants.y .= land_size
+        elseif immigrant_edges == "east"
+            immigrants.x .= land_size
+            immigrants.y = rand(1:land_size, n_new)
+        elseif immigrant_edges == "south"
+            immigrants.x = rand(1:land_size, n_new)
+            immigrants.y .= 1
+        else 
+            immigrants.x .= 1
+            immigrants.y = rand(1:land_size, n_new)
+        end
+    end
+
+    # Pick a direction from list of inline functions
+    upleft(x,y)=[x-1, y+1]; up(x,y)=[x, y+1]; upright(x,y)=[x+1, y+1]
+    left(x,y)=[x-1, y]; right(x,y)=[x+1, y]
+    downleft(x,y)=[x-1, y-1]; down(x,y)=[x, y-1]; downright(x,y)=[x+1, y-1]
+    directions = rand([upleft, up, upright, left, right, 
+                    downleft, down, downright], size(immigrants,1))
+
+    # Get movement distance
+    distances = rand(Poisson(10), size(immigrants,1))
+
+    # RUN!
+    coords = Vector(undef, size(immigrants,1))
+
+    for i in 1:length(distances)
+        coords[i] = [immigrants.x[i], immigrants.y[i]]
+        for j in 1:distances[i]
+            coords[i] = directions[i](coords[i][1], coords[i][2])
+        end
+    end
+
+    # Keep immigrants that stayed in the landscape
+    immigrants.x = [coords[i][1] for i in 1:length(coords)]
+    immigrants.y = [coords[i][2] for i in 1:length(coords)]
+    filter!([:x, :y] => (x,y) -> 0 < x <= land_size && 0 < y <= land_size, immigrants)
+
+    # Append immigrants to main dataset & home coords
+    dat = [dat;immigrants]
+    home = [home;DataFrame(id = immigrants.id, x = immigrants.x, y = immigrants.y)]
+end
--- figures_prelim.R
+++ figures_prelim.R
@@ -12,16 +12,21 @@ library(quantreg)
 # Load data --------------------
 setwd("c:/users/beasl/documents/urban-rabies-sim")
 sero_outs <- read.csv("outputs_prelim.csv")
+sero_extended <- read.csv("outputs_extended.csv")
 
 # Clean data -----------------------------
 # Add seroprevalence column
-nrep = 50
-nyear = 10
-nweek = 52
+nrep <- 50
+nyear <- 10
+nweek <- 52
 
-seros = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8)
+seros <- c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8)
+seros_extended <- c(0.05, 0.1, 0.15, 0.55, 0.65, 0.75)
 
-sero_outs$sero = rep(seros, each = nrep*nyear*nweek)
+sero_outs$sero <- rep(seros, each = nrep*nyear*nweek)
+sero_extended$sero <- rep(seros_extended, each = nrep*nyear*nweek)
+
+sero_outs <- rbind(sero_outs, sero_extended)
 
 sero_time <- sero_outs %>%
   mutate(sero = factor(sero)) %>%
@@ -36,19 +41,23 @@ ggplot(data = sero_time, aes(x = sero, y = nweek))+
   theme_bw()+
   theme(panel.grid = element_blank())
 
-ggsave(filename = "outbreakduration.jpeg", width = 5, height = 4,
-       units = "in", dpi = 600)
+# ggsave(filename = "outbreakduration.jpeg", width = 5, height = 4,
+#        units = "in", dpi = 600)
 
-summary(lm(data = sero_time, formula = nweek~as.numeric(sero)))
+summary(lm(data = sero_time, 
+           formula = nweek~as.numeric(as.character(sero))))
 
-summary(rq(data = sero_time, formula = nweek~as.numeric(sero),
-           tau = c(0.1, 0.25, 0.5, 0.75, 0.9)))
+summary(rq(data = sero_time, 
+           formula = nweek~as.numeric(as.character(sero)),
+           tau = c(0.05, 0.25, 0.5, 0.75, 0.95)))
 
-rq.frame <- tibble(slopes = c(-2.25, -2.667, -5.167, -7, -16.667),
-                      inters = c(80.25, 88, 116.5, 155, 252.333),
-                      quants = c("10%", "25%", "50%", "75%", "90%"))
+rq.frame <- tibble(slopes = c(-20, -24.44, -38, -29.33, -140),
+                      inters = c(75, 84.33, 105.8, 127, 248),
+                      quants = c("5%", "25%", "50%", "75%", "95%"))%>%
+  mutate(quants = factor(quants, levels = quants))
 
-ggplot(data = sero_time, aes(x = as.numeric(sero), y = nweek))+
+ggplot(data = sero_time, aes(x = as.numeric(as.character(sero)), 
+                             y = nweek))+
   geom_point()+
   geom_abline(data = rq.frame, aes(intercept=inters, slope = slopes,
                                    color = quants))+
@@ -57,25 +66,27 @@ ggplot(data = sero_time, aes(x = as.numeric(sero), y = nweek))+
   theme_bw()+
   theme(panel.grid = element_blank())
 
-ggsave(filename = "quantileoubreakduration.jpeg", width = 5, 
-       height = 4, units = "in", dpi = 600)
+# ggsave(filename = "quantileoubreakduration.jpeg", width = 5, 
+#        height = 4, units = "in", dpi = 600)
   
 # max weekly cases
 infecs <- sero_outs %>%
-  mutate(sero = factor(sero)) %>%
+  # mutate(sero = factor(sero)) %>%
   group_by(rep, sero) %>%
   filter(n_infected != 0 | n_symptomatic != 0) %>%
   group_by(sero, rep) %>%
   summarise(max = max(n_infected))
 
-ggplot(data = infecs, aes(x = sero, y = max))+
+summary(lm(max~sero, data = infecs))
+
+ggplot(data = infecs, aes(x = factor(sero), y = max))+
   geom_boxplot(fill = "lightgray")+
   labs(x = "Seroprevalence", y = "Maximum Weekly Infections")+
   theme_bw()+
   theme(panel.grid = element_blank())
 
-ggsave(filename = "maxcases.jpeg", width = 5, height = 4, 
-       units = 'in', dpi = 600)
+# ggsave(filename = "maxcases.jpeg", width = 5, height = 4, 
+#        units = 'in', dpi = 600)
 
 # mean cases per week
 infecs_time <- sero_outs %>%
@@ -139,12 +150,22 @@ sero_death_time <- sero_smol %>%
   group_by(sero, nweek) %>%
   summarise(mean = mean(deaths, na.rm = T))
 
+birth_pulse <- c(20, (20+52), 20+(2*52), 20+(3*52), 20+(4*52),
+                 20+(5*52), 20+(6*52), 20+(7*52), 20+(8*52))
+
+juvie_dispersal <- c(50+(2*52), 50+(3*52))
+
 ggplot(sero_death_time, aes(x = nweek, y = mean, color=sero))+
   geom_line()+
+  geom_vline(xintercept = birth_pulse, linetype = "dashed",
+             alpha = 0.5)+
+  geom_vline(xintercept = juvie_dispersal, color = "red",
+             linetype = "dashed", alpha = 0.5)+
   scale_color_viridis_d(name = "Seroprevalence")+
   labs(x = "Week", y = "Mean Deaths")+
   theme_bw()+
   theme(panel.grid = element_blank())
 
-ggsave(filename = "deathsperweek.jpeg", width = 5, height = 4,
-       units = 'in', dpi = 600)
+# ggsave(filename = "deathsperweek_juvie_dispersal.jpeg", width = 5, 
+#        height = 4, units = 'in', dpi = 600)
+
