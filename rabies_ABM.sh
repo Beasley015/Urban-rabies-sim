3ff65533db1bebad657e93a47eb21a73415159c5
Beasley015 <beasley.em@gmail.com>
2025-03-26 08:43:55 -0400

    more results

--- rabies_ABM.sh
+++ rabies_ABM.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-#SBATCH --array=1-450
-#SBATCH --time=23:00:00
+#SBATCH --array=166-450
+#SBATCH --time=18:00:00
 #SBATCH --cpus-per-task=1 
 #SBATCH --account=ctb-tpoisot
 #SBATCH --output=slurm\%x-%a.out 
-#SBATCH --mem-per-cpu=245G
+#SBATCH --mem-per-cpu=150G
 #SBATCH --mail-user=emily.beasley@umontreal.ca
 #SBATCH --mail-type=ALL
 
